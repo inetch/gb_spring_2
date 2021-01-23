0a49cb2bcef5ffa6627fbb6d44bdac36422994b7
Ivan Necheukhin <necheukhin@bpcbt.com>
2021-01-23 20:24:57 +0300

    initial

---
+++ shoppee/src/main/java/ru/gb/inetch/shoppee/ShoppeeApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.inetch.shoppee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppeeApplication.class, args);
+    }
+
+}
---
+++ shoppee/src/test/java/ru/gb/inetch/shoppee/ShoppeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.inetch.shoppee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
